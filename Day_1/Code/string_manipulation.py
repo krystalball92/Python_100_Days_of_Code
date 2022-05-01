790ab381dc19eed429809ddcbbb1f81c79fd83d7
krystalball92 <104739803+krystalball92@users.noreply.github.com>
2022-05-01 16:38:52 -0700

    Add files via upload

---
+++ Day_1/Code/string_manipulation.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+
+# cant indent - start code at the begining of the line - no spaces allowed 
+
+# \n creates break onto second line 
+print("Hello world! \nHello world")
+
+# String Concatenation - merging 2 strings into 1 
+print("Hello" + " Krystal")
