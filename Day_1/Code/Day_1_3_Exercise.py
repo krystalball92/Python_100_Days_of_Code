3aaaed3167dba4729c7dc85449c7c3ddb4cfc027
krystalball92 <104739803+krystalball92@users.noreply.github.com>
2022-05-01 17:46:07 -0700

    Add files via upload

---
+++ Day_1/Code/Day_1_3_Exercise.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+
+#input function requests user response and prints without having to use print function
+
+# assigning the input response to a variable 
+
+SizeOfName = input("What is your name?")
+
+# you print the size of the variable
+print(len(SizeOfName))
+
+
+
+
+
+
