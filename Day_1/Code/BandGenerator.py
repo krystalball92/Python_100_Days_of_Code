aefb20a96ec5ba3556fe68639e98674adb1cfa42
krystalball92 <104739803+krystalball92@users.noreply.github.com>
2022-05-08 12:24:30 -0700

    Add files via upload

---
+++ Day_1/Code/BandGenerator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Let's start a band!")
+#2. Ask the user for favorite food
+Food = input("What is your favorite food?\n")
+#3. Ask the user for color a word that describes them
+Describe = input("Describe yourself in one word.\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is " + Describe + " " + Food)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
